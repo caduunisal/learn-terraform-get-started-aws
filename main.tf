391db442ad9bdf7f27f134aa074284fa3589c002
caduunisal <kadu.unisal1@gmail.com>
2025-07-23 14:14:27 -0300

    More SSH configs

--- main.tf
+++ main.tf
@@ -14,14 +14,23 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws-ec2-key"
+  public_key = file("${path.module}/aws-ec2-key.pub")
+}
+
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
 
   tags = {
     Name = "learn-terraform"
   }
 }
+
+
+
 
 
 

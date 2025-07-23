4ddc62f8baab8f5d48844abaa13c0b837cb88b27
caduunisal <kadu.unisal1@gmail.com>
2025-07-23 14:29:12 -0300

    Add aws_security_group module

--- main.tf
+++ main.tf
@@ -29,6 +29,35 @@ resource "aws_instance" "app_server" {
   }
 }
 
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "Permite SSH e HTTP"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # ou mais seguro: seu IP apenas
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 
 
 
